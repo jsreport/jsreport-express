e0a733f16da55d141512bcca711afc17277cb589
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-01-15 16:56:36 +0100

    adapt to jsreport-core v2

--- test/expressTest.js
+++ test/expressTest.js
@@ -1,69 +1,50 @@
 var supertest = require('supertest')
 var Reporter = require('jsreport-core')
 
-describe('express', function () {
-  var reporter
+describe('express', () => {
+  let reporter
 
-  beforeEach(function () {
+  beforeEach(() => {
     reporter = Reporter().use(require('../')()).use(require('jsreport-jsrender')())
     return reporter.init()
   })
 
-  it('/api/settings should return 200', function (done) {
-    supertest(reporter.express.app)
+  afterEach(() => reporter.close())
+
+  it('/api/settings should return 200', () => {
+    return supertest(reporter.express.app)
       .get('/api/settings')
-      .expect(200, done)
+      .expect(200)
   })
 
-  it('/api/recipe should return 200', function (done) {
-    supertest(reporter.express.app)
+  it('/api/recipe should return 200', () => {
+    return supertest(reporter.express.app)
       .get('/api/recipe')
-      .expect(200, done)
+      .expect(200)
   })
 
-  it('/api/version should return a package.json version', function (done) {
-    supertest(reporter.express.app)
+  it('/api/version should return a package.json version', () => {
+    return supertest(reporter.express.app)
       .get('/api/version')
       .expect(200, reporter.version)
-      .end(function (err, res) {
-        if (err) {
-          done(err)
-        } else {
-          done()
-        }
-      })
   })
 
-  it('/api/report should render report', function (done) {
-    supertest(reporter.express.app)
+  it('/api/report should render report', () => {
+    return supertest(reporter.express.app)
       .post('/api/report')
       .send({template: {content: 'Hey', engine: 'none', recipe: 'html'}})
       .expect(200, 'Hey')
-      .end(function (err, res) {
-        if (err) {
-          done(err)
-        } else {
-          done()
-        }
-      })
   })
 
-  it('/api/report should parse data if string and  render report', function (done) {
-    supertest(reporter.express.app)
+  it('/api/report should parse data if string and  render report', () => {
+    return supertest(reporter.express.app)
       .post('/api/report')
       .send({template: {content: '{{:a}}', engine: 'jsrender', recipe: 'html'}, data: '{ "a": "foo" }'})
       .expect(200, 'foo')
-      .end(function (err, res) {
-        if (err) {
-          done(err)
-        } else {
-          done()
-        }
-      })
   })
 
-  it('/api/report should use options[Content-Disposition] if set', function (done) {
-    supertest(reporter.express.app)
+  it('/api/report should use options[Content-Disposition] if set', () => {
+    return supertest(reporter.express.app)
       .post('/api/report')
       .send({
         template: {content: '{{:a}}', engine: 'jsrender', recipe: 'html'},
@@ -72,18 +53,11 @@ describe('express', function () {
       })
       .expect(200, 'foo')
       .expect('Content-Disposition', 'foo')
-      .end(function (err, res) {
-        if (err) {
-          done(err)
-        } else {
-          done()
-        }
-      })
   })
 
-  it('/odata/$metadata should return 200', function (done) {
-    supertest(reporter.express.app)
+  it('/odata/$metadata should return 200', () => {
+    return supertest(reporter.express.app)
       .get('/odata/$metadata')
-      .expect(200, done)
+      .expect(200)
   })
 })
