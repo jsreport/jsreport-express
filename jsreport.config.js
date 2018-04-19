2c287e23a96f937d4c762e154108ac2971dd977f
BJR Matos <bjrmatos@gmail.com>
2018-04-19 18:05:51 -0500

    add optionsSchema and make readOnly value of context from sandbox

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,4 +1,32 @@
+
+const schemaProperties = {
+  hostname: { type: 'string' },
+  httpPort: { type: 'number' },
+  httpsPort: { type: 'number' },
+  certificate: {
+    type: 'object',
+    properties: {
+      key: { type: 'string' },
+      cert: { type: 'string' }
+    }
+  }
+}
+
 module.exports = {
   'name': 'express',
-  'main': 'lib/reporter.express.js'
+  'main': 'lib/reporter.express.js',
+  'optionsSchema': {
+    ...schemaProperties,
+    extensions: {
+      express: {
+        type: 'object',
+        properties: {
+          ...schemaProperties,
+          trustProxy: { type: 'boolean' },
+          inputRequestLimit: { type: 'string' },
+          renderTimeout: { type: 'number' }
+        }
+      }
+    }
+  }
 }
